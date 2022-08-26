a2f2f535b0a6c6ffd5251538df0163fbb767ca4e
Patrick Noonan <Prnoonan11@gmail.com>
2022-08-25 19:22:14 -0500

    pet types enum

--- FurryFriends.Data/Entities/UserEntity.cs
+++ FurryFriends.Data/Entities/UserEntity.cs
@@ -8,10 +8,39 @@ namespace FurryFriends.Data.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
-        public string PetType { get; set; }
+        public virtual int PetType
+        {
+            get
+            {
+                return (int)this.PetTypeEnum;
+            }
+            set
+            {
+                PetType = (int)(PetTypes)value;
+            }
+        }
         public string Breed { get; set; }
         public string Bio { get; set; }
         public string Size { get; set; }
         public string City { get; set; }
+
+        [EnumDataType(typeof(PetTypes))]
+        public PetTypes PetTypeEnum { get; set; }
     }
+
+
+
+
+    public enum PetTypes
+    {
+        Dog = 0,
+        Cat = 1,
+        Hampster = 2,
+        Chicken = 3,
+        Lobster = 4,
+        Fish = 5,
+        Bird = 6,
+        Other = 7
+    }
+
 }
