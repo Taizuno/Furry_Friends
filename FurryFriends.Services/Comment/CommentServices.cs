e7f4384cefacee42901155fa1268ed81d0ba4205
Braeden McNealy <lightning9501@gmail.com>
2022-09-13 17:34:19 -0400

    dumb

--- FurryFriends.Services/Comment/CommentServices.cs
+++ FurryFriends.Services/Comment/CommentServices.cs
@@ -2,21 +2,26 @@ using FurryFriends.Data;
 using FurryFriends.Data.Entities;
 using FurryFriends.Models.Comment;
 using System.Threading.Tasks;
-using System;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
-
+using AutoMapper;
+using FurryFriends.Services.Wrapper;
+using Microsoft.AspNetCore.Http;
 namespace FurryFriends.Services.Comment
 {
     public class CommentServices : ICommentServices
     {
+        private readonly int _postID;
+        private readonly IMapper _mapper;
         private readonly ApplicationDbContext _DbContext;
-        public CommentServices(ApplicationDbContext DbContext)
+        public CommentServices(ApplicationDbContext DbContext, IMapper mapper)
         {
             _DbContext = DbContext;
+            _mapper = mapper;
         }
         public async Task<bool> CreateCommentAsync(CommentCreate model)
         {
-            if (await GetCommentByUserNameAsync(model.UserName) != null)
+            if (await GetCommentByIDAsync(model.Id) != null)
                 return false;
             var entity = new CommentEntity
             {
@@ -33,13 +38,50 @@ namespace FurryFriends.Services.Comment
 
             return numberOfChanges == 1;
         }
-        private async Task<CommentEntity> GetCommentByUserNameAsync(string Username)
+        public async Task<CommentListItem> GetCommentByIDAsync(int commentId)
         {
-            var entity = await _DbContext.Comment.FirstOrDefaultAsync(e => e.UserName == Username);
+            var comment = await _DbContext.Comment.FirstOrDefaultAsync(e => e.Id == commentId);
 
-            return entity is null ? null : 
+            return _mapper.Map<CommentListItem>(comment);
             
         }
+        public async Task<IEnumerable<CommentListItem>> GetCommentsByUserNameAsync(PaginationFilter _filter, HttpContext httpContext, string Username)
+        {
+            var comments = await _DbContext.Comment
+                .Select(entity => _mapper.Map<CommentListItem>(entity));
+
+            var pMetadata = new PaginationMetaData(comments.Count(),
+            _filter.CurrentPage, _filter.PageSize);
+            httpContext.Response.Headers.Add("Pagination", JsonSerializer.Serialize(pMetadata));
+
+            var list = await comments.Skip((_filter.CurrentPage -1) * _filter.PageSize)
+                .Take(_filter.PageSize)
+                .ToListAsync();
+        }
         
+        public async Task<bool> UpdateCommentAsync(CommentUpdate request)
+        {
+            var commentUserCheck = await _DbContext.Comment.AnyAsync(comment => comment.Id == request.Id && comment.PostId == _postID);
+
+            if (!commentUserCheck)
+            {
+                return false;
+            }
+
+            var updatedComment = _mapper.Map<CommentUpdate, CommentEntity>(request, opt => opt.AfterMap((src, dest) => dest.PostId = _postID));
+            _DbContext.Entry(updatedComment).State = EntityState.Modified;
+            _DbContext.Entry(updatedComment).Property(e => e.DateTimeCreated).IsModified = false;
+            var numberOfChanges = await _DbContext.SaveChangesAsync();
+
+            return numberOfChanges == 1;
+        }
+
+        public async Task<bool> DeleteCommentAsync(int commentId)
+        {
+            var commentDelete = await _DbContext.Comment.FindAsync(commentId);
+
+            _DbContext.Comment.Remove(commentDelete);
+            return await _DbContext.SaveChangesAsync() == 1;
+        }
     }
 }
--- FurryFriends.Services/Comment/ICommentServices.cs
+++ FurryFriends.Services/Comment/ICommentServices.cs
@@ -1,11 +1,16 @@
 using System.Threading.Tasks;
 using FurryFriends.Models.Comment;
 using FurryFriends.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using FurryFriends.Services.Wrapper;
 namespace FurryFriends.Services.Comment
 {
     public interface ICommentServices
     {
          Task<bool> CreateCommentAsync(CommentCreate model);
-         Task<CommentEntity> GetCommentbyUserNameAsync(string UserName);
+         Task<CommentListItem> GetCommentbyIDAsync(int commentID);
+         Task<IEnumerable<CommentListItem>> GetCommentsbyUserNameAsync(PaginationFilter _filter, HttpContext httpContext, string Username);
+         Task<bool> UpdateCommentAsync(CommentUpdate request);
+         Task<bool> DeleteCommentAsync(int commentID);
     }
 }
